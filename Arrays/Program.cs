f045e26c9d40b543b5292164ae2dac0d0598c4cc
Olcaydeniz <olcaydenizk@gmail.com>
2024-02-27 22:37:37 +0300

    Arrys end.

---
+++ Arrays/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Array
+            //string[] students = new string[3];
+            ////students[0] = "Olcay";
+            ////students[1] = "Deniz";
+            ////students[2] = "Engin";
+
+            //string[] students2 = new string[] {"Olcay","Deniz","Engin" };
+            //string[] students3 = { "Olcay", "Deniz", "Engin" };
+
+
+            //foreach (var student in students) 
+            //{
+            //    Console.WriteLine(student);
+            //}
+
+            //Çok Boyutlu Diziler
+            string[,] regions = new string[5,3]
+            {
+                { "İstanbul","İzmit", "Balıkesir"},
+                { "Ankara","Konya","Kırıkkale"},
+                { "Antalya","Adana","Mermsin"},
+                { "Rize","Trabzon","Samsun"},
+                { "İzmir","Muğla","Manisa"}
+            };
+            for (int i = 0; i <= regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i, j]);
+                }
+                Console.WriteLine("***********");
+            }
+;
+            Console.WriteLine();
+            Console.ReadLine(); 
+
+        }
+    }
+}
