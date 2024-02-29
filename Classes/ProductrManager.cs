32280a62bf99256df5ddb9032d9380d04bf1fbcc
DENIZ <DENIZ@LAPTOP-9PQA1LOL>
2024-02-29 13:15:51 +0300

    Classes .Class

---
+++ Classes/ProductrManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class ProductrManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Product Added!");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Product Updated!");
+
+        }
+
+
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();   
+
+            ProductrManager productrManager = new ProductrManager();    
+            productrManager.Add();
+            productrManager.Update();
+
+            Customer customer = new Customer();
+            customer.Id =  1 ;
+            customer.FirtName = "Olcay";
+            customer.LastName = "Deniz";
+            customer.City = "Adana";
+
+            Customer customer2 = new Customer
+            {
+                Id = 2 ,FirtName="Ol",LastName="De",City="Ada"
+            };
+            Console.WriteLine(customer2.FirtName);   
+
+            Console.ReadLine();
+        }
+
+        
+
+      
+
+
+    }
+}
